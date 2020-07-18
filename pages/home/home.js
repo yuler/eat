2e869db69b5ade78d4a7bb3aea2cbceaedb2265a
yuler <joyful4yu@gmail.com>
2020-07-18 21:53:39 +0800

    Simple design home page UI

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,15 @@
 Page({
-  
+  data: {
+    restaurant: {
+      name: '港真茶餐厅(亮马桥店)',
+      cover: 'http://p1.meituan.net/bbia/68792930_1590026403194.jpeg',
+      posters: [
+
+      ],
+      recommend: "茶餐厅，烤鸭，肠粉贼不错。",
+    }
+  },
+  submit() {
+    console.log('submit')
+  }
 })
