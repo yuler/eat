daff5787101d44ef087233e1ec7022d07d88c5c0
yuler <joyful4yu@gmail.com>
2020-07-20 22:47:57 +0800

    Add `about` button in home page

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,12 +2,25 @@ const Api = require('../../api')
 
 Page({
   data: {
-    restaurant: null
+    restaurant: null,
+    aboutPosition: null,
+  },
+
+  onLoad() {
+    const { screenWidth, screenHeight } = wx.getSystemInfoSync()
+    const ratio = screenWidth / 375
+    const offset = 50 * ratio + 20
+    this.setData({ aboutPosition: { x: screenWidth - offset, y: screenHeight - offset }})
   },
 
   async submit() {
     wx.vibrateShort()
     const restaurant = await Api.fetchRandomRestaurant()
     this.setData({ restaurant })
+  },
+
+  moveEnd(event) {
+    const { aboutPosition } = this.data
+    this.setData({ aboutPosition })
   }
 })
