02d5d6efd65c799ac04f07fc8fa47c92f95e30ab
yuler <joyful4yu@gmail.com>
2020-07-20 21:41:20 +0800

    Fix `scripts/deploy` add try catch & upload `settings`

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,25 +6,37 @@ const pkg = require('../package.json')
 const APP_ID = 'wxda5fb4f5808e2104'
 
 ;(async () => {
-  const project = new ci.Project({
-    appid: APP_ID,
-    type: 'miniProgram',
-    projectPath: path.resolve(__dirname, '../'),
-    privateKeyPath: path.resolve(__dirname, `../private.${APP_ID}.key`),
-    ignores: [
-      'node_modules/**/*',
-      'database/**/*',
-      'scripts/**/*',
-    ],
-  })
-  const uploadResult = await ci.upload({
-    project,
-    version: pkg.version,
-    desc: process.env.APP_DESCRIPTION || pkg.description,
-    setting: {
-      es6: true,
-    },
-    onProgressUpdate: console.log,
-  })
-  console.log(uploadResult)
+  try {
+    const project = new ci.Project({
+      appid: APP_ID,
+      type: 'miniProgram',
+      projectPath: path.resolve(__dirname, '../'),
+      privateKeyPath: path.resolve(__dirname, `../private.${APP_ID}.key`),
+      ignores: [
+        'node_modules/**/*',
+        'database/**/*',
+        'scripts/**/*',
+      ],
+    })
+    const uploadResult = await ci.upload({
+      project,
+      version: pkg.version,
+      desc: process.env.APP_DESCRIPTION || pkg.description,
+      setting: {
+        es6: true,
+        es7: true,
+        minifyJS: true,
+        minifyWXML: true,
+        minifyWXSS: true,
+        minify: true,
+        codeProtect: true,
+        autoPrefixWXSS: true,
+      },
+      onProgressUpdate: console.log,
+    })
+    console.log(uploadResult)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
 })()
